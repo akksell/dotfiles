3ee9e14eb81ff3e9269cfd1cc1bbf28d9a9eb2b2
akksell <axel.ramone@gmail.com>
2025-12-29 23:45:48 -0600

    fix: configure lsp support
    I originally forked this config from Primeagen. Mason-lsp has been
    updated since then and it wasn't working on my machine so I updated to
    the latest version (the owner of the repo also changed).
    
    The config also used a different completion plugin that required a ton
    of dependencies. I've switched it over to blink since it uses a rust
    fuzzy finder. Alot of the key mappings are also removed with this
    change.

--- .config/nvim/lua/silly/plugins/lsp.lua
+++ .config/nvim/lua/silly/plugins/lsp.lua
@@ -1,121 +1,31 @@
 return {
     "neovim/nvim-lspconfig",
+    opts = {},
     dependencies = {
-        "stevearc/conform.nvim",
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
+        { "mason-org/mason.nvim", opts = {} },
+        "mason-org/mason-lspconfig.nvim",
+        "saghen/blink.cmp",
     },
-
-    config = function()
-        require("conform").setup({
-            formatters_by_ft = {
-            }
-        })
-        local cmp = require('cmp')
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities())
-        local on_attach = function(client, bufnr)
-            local bufopts = { noremap = true, silent = true, buffer = bufnr }
-            vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, bufopts)
-        end
-
-        require("fidget").setup({})
-        require("mason").setup()
+    config = function(_, opts)
+        -- enable/install specific LSPs
         require("mason-lspconfig").setup({
-            automatic_enable = false,
             ensure_installed = {
                 "lua_ls",
                 "rust_analyzer",
                 "gopls",
                 "vtsls",
             },
-            handlers = {
-                function(server_name) -- default handler (optional)
-                    require("lspconfig")[server_name].setup {
-                        on_attach = on_attach,
-                        capabilities = capabilities
-                    }
-                end,
+        });
 
-                zls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.zls.setup({
-                        on_attach = on_attach,
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
-                            },
-                        },
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
-
-                end,
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup {
-                        on_attach = on_attach,
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                                }
-                            }
-                        }
-                    }
-                end,
-            }
-        })
-
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
-
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
+        -- configure completion
+        require("blink.cmp").setup({
+            fuzzy = {
+                implementation = "rust",
+                prebuilt_binaries = {
+                    download = true,
+                    force_version = "v1.8.0"
+                },
             },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-                ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-                ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                ["<C-Space>"] = cmp.mapping.complete(),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-            }, {
-                { name = 'buffer' },
-            })
-        })
-
-        vim.diagnostic.config({
-            -- update_in_insert = true,
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-            },
-        })
+        });
     end
 }
