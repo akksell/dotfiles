a948d756713caf5d89b3328d1ddf7e5026231f4f
akksell <axel.ramone@gmail.com>
2025-12-31 15:09:08 -0600

    chore: use relative line numbers in nvim
    I'm trying to get better at vim motions and I think relative line
    numbers will be more helpful to do that by using jump commands rather
    than having to remeber a specific line to jump to

--- .config/nvim/lua/silly/set.lua
+++ .config/nvim/lua/silly/set.lua
@@ -4,6 +4,6 @@ vim.opt.expandtab = true -- use spaces instead of tab character
 vim.opt.softtabstop = 4 -- number of spaces inserted instead of tab character
 vim.opt.shiftwidth = 4 -- number of spaces inserted when indenting
 
-vim.opt.number = true
+vim.opt.relativenumber = true
 
 vim.opt.colorcolumn = "80"
